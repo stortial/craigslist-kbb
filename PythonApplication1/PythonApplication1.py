f4cce3ff4e830ca49711d525e23c6db1bd122b28
stortial <adstorti@gmail.com>
2017-12-24 23:34:05 -0500

    got the title for the car and parsed the year

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -8,8 +8,8 @@ def getLinks():
 	#website were going to
 	craig = "https://binghamton.craigslist.org/search/cta?hasPic=1&searchNearby=1"
 
+	#opens the webpage and stores it in soup
 	page = urllib2.urlopen(craig)
-
 	soup = BeautifulSoup(page, 'html.parser')
 	
 	#get each individual post
@@ -25,10 +25,24 @@ def getLinks():
 
 def iterate(u):
 	page = urllib2.urlopen(u)
+	#gets the webpage nad stores it as soup
 	soup = BeautifulSoup(page, 'html.parser')
-	list = soup.select('p.attrgroup')
-	print list
+
+	#gets the list of things on the side and saves it under list
+	list = soup.find_all("p", "attrgroup")
 	
+	#get the title ex "2008 gmc 1500 sierra" and expunge it of the whitespace
+	title = list[0].text
+	title = title.strip()
+
+	parse(title)
+
+	#print title
+	
+def parse(t):
+	#find the year
+	test = re.match(r'\d\d\d\d',t)
+	print test.group()
 
 file = open('output.txt','w')
 
@@ -39,7 +53,7 @@ iterate(theLinks[0])
 #for e in theLinks:
 	#iterate(e)
 
-
+	 
 
 
 
