0a78b5f7b84082c4230c69167909a689d53851d2
stortial <adstorti@gmail.com>
2019-01-03 18:49:38 -0500

    finished list of all makes and models by year

--- gets.py
+++ gets.py
@@ -4,6 +4,8 @@ from selenium.webdriver.common.keys import Keys
 from time import sleep
 import sys
 from selenium.webdriver.support.select import Select
+import pickle
+import os
 
 #from ghost import Ghost
 
@@ -15,18 +17,29 @@ sleep(1)#need to sleep while waiting for webpage
 f = open('out.txt','w')
 sys.stdout = f
 
+#initialize dict
 mnmbyYear = {}
+
+#check to see if we can load the old dict
+if os.path.isfile('C:/Users/adam/Documents/github/craigslist-kbb/dict.pickle'):
+    with open('dict.pickle', 'rb') as handle:
+        mnmbyYear = pickle.load(handle)
 
 #moves curser to year and starts for loop
 walker = browser.find_element_by_xpath("//*[@id='yearDropdown0']")
 walker.click()
 years = walker.find_elements_by_tag_name('option')
 
-for year in years:
-    mnmbyYear[year.text] = {}
+for year in years[1:]:
 
     walker.send_keys(Keys.ARROW_DOWN)
     walker.send_keys(Keys.ENTER)
+    print(year.text)
+    if mnmbyYear:
+        if year.text in mnmbyYear.keys():
+            continue
+
+    mnmbyYear[year.text] = {}
 
     maker = browser.find_element_by_xpath("//*[@id='makeDropdown0']")
     maker.click()
@@ -42,13 +55,18 @@ for year in years:
         maker.send_keys(Keys.ENTER)
 
         #now we can check all the models
+        sleep(1)
         modeler = browser.find_element_by_xpath("//*[@id='modelDropdown0']")
+        sleep(1)
         modeler.click()
+        sleep(1)
         models = modeler.find_elements_by_tag_name('option')
 
         theModels = modeler.text.splitlines()
         theModels.pop(0)
 
         mnmbyYear[year.text][make.text] = theModels
-        #print(make.text, theModels)
-    print(mnmbyYear[year.text])
+
+    #save the dict
+    with open('dict.pickle', 'wb') as handle:
+        pickle.dump(mnmbyYear, handle, protocol=pickle.HIGHEST_PROTOCOL)
